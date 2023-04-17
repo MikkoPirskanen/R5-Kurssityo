04d6c78b01f0406b16a8be43bf69b515f673e071
Mikko <mikko.a.mottonen@edu.savonia.fi>
2023-04-17 22:38:52 +0300

    varausta

--- Varaukset.Designer.cs
+++ Varaukset.Designer.cs
@@ -28,18 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            Mökit = new Label();
+            listBox2 = new ListBox();
+            varausKalenteri = new MonthCalendar();
+            comboBox1 = new ComboBox();
+            dataGridView1 = new DataGridView();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(75, 96);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Alueet";
+            // 
+            // Mökit
+            // 
+            Mökit.AutoSize = true;
+            Mökit.Location = new Point(286, 96);
+            Mökit.Name = "Mökit";
+            Mökit.Size = new Size(38, 15);
+            Mökit.TabIndex = 2;
+            Mökit.Text = "Mökit";
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(203, 136);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(167, 199);
+            listBox2.TabIndex = 4;
+            listBox2.SelectedIndexChanged += listBox2_SelectedIndexChanged;
+            // 
+            // varausKalenteri
+            // 
+            varausKalenteri.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            varausKalenteri.ForeColor = SystemColors.WindowText;
+            varausKalenteri.Location = new Point(112, 485);
+            varausKalenteri.MaxDate = new DateTime(2028, 12, 31, 0, 0, 0, 0);
+            varausKalenteri.MinDate = new DateTime(2023, 1, 1, 0, 0, 0, 0);
+            varausKalenteri.Name = "varausKalenteri";
+            varausKalenteri.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Etelä-Savo", "Keski-Suomi", "Pohjois-Savo" });
+            comboBox1.Location = new Point(53, 136);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 6;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged_1;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(407, 136);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(545, 390);
+            dataGridView1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(597, 96);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Mökin tiedot";
             // 
             // Varaukset
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(984, 761);
+            Controls.Add(label2);
+            Controls.Add(dataGridView1);
+            Controls.Add(comboBox1);
+            Controls.Add(varausKalenteri);
+            Controls.Add(listBox2);
+            Controls.Add(Mökit);
+            Controls.Add(label1);
             Name = "Varaukset";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Varaukset";
+            Load += Varaukset_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label label1;
+        private Label Mökit;
+        private ListBox listBox2;
+        private MonthCalendar varausKalenteri;
+        private ComboBox comboBox1;
+        private DataGridView dataGridView1;
+        private Label label2;
     }
 }
--- Varaukset.cs
+++ Varaukset.cs
@@ -12,9 +12,82 @@ namespace Mökkihöperö
 {
     public partial class Varaukset : Form
     {
+
         public Varaukset()
         {
             InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            {
+                // Tyhjennä listboxi
+                listBox2.Items.Clear();
+
+
+
+                // Hae valitun alueen mökit
+                string selectedArea = comboBox1.SelectedItem.ToString();
+                string[] cabins = null;
+
+                if (selectedArea == "Pohjois-Savo")
+                {
+                    cabins = new string[] { "Mökki 1", "Mökki 2", "Mökki 3" };
+                }
+                else if (selectedArea == "Etelä-Savo")
+                {
+                    cabins = new string[] { "Mökki 4", "Mökki 5", "Mökki 6" };
+                }
+
+                // Lisää mökit listboxiin
+                if (cabins != null)
+                {
+                    foreach (string cabin in cabins)
+                    {
+                        listBox2.Items.Add(cabin);
+                    }
+                }
+            }
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Dictionary<string, Dictionary<string, string>> cabins = new Dictionary<string, Dictionary<string, string>>();
+            cabins.Add("Mökki 1", new Dictionary<string, string>() { { "Osoite", "Katu 1" }, { "Hinta", "100€/vrk" }, { "Kuvaus", "Tunnelmallinen hirsimökki" } });
+            cabins.Add("Mökki 2", new Dictionary<string, string>() { { "Osoite", "Katu 2" }, { "Hinta", "150€/vrk" }, { "Kuvaus", "Iso ja moderni huvila" } });
+            cabins.Add("Mökki 3", new Dictionary<string, string>() { { "Osoite", "Katu 3" }, { "Hinta", "200€/vrk" }, { "Kuvaus", "Tyylikäs huvila" } });
+            cabins.Add("Mökki 4", new Dictionary<string, string>() { { "Osoite", "Katu 4" }, { "Hinta", "250€/vrk" }, { "Kuvaus", "Tyylikäs huvila" } });
+            cabins.Add("Mökki 5", new Dictionary<string, string>() { { "Osoite", "Katu 5" }, { "Hinta", "300€/vrk" }, { "Kuvaus", "Tyylikäs huvila" } });
+            cabins.Add("Mökki 6", new Dictionary<string, string>() { { "Osoite", "Katu 6" }, { "Hinta", "350€/vrk" }, { "Kuvaus", "Tyylikäs huvila" } });
+
+
+            // Hae valitun mökin tiedot
+            string selectedCabin = listBox2.SelectedItem.ToString();
+            Dictionary<string, string> cabinInfo = cabins[selectedCabin];
+
+            // Näytä tiedot datagridviewissä
+            dataGridView1.Rows.Clear();
+            foreach (KeyValuePair<string, string> kvp in cabinInfo)
+            {
+                dataGridView1.Columns.Add(kvp.Key, kvp.Value);
+                dataGridView1.Rows.Add(kvp.Key, kvp.Value);
+                         
+            }
+        }
+
+        
+
+
+
+private void Varaukset_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
