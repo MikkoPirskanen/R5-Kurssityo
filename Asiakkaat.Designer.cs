0fab57bbbd7b28c7e0a4730d7d10b9250d8415cc
MikkoPirskanen <129946559+MikkoPirskanen@users.noreply.github.com>
2023-04-18 22:59:36 +0300

    Update Asiakkaat.Designer.cs

--- Asiakkaat.Designer.cs
+++ Asiakkaat.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Mökkihöperö
+namespace Mökkihöperö
 {
     partial class Asiakkaat
     {
@@ -28,19 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            haeNappi = new Button();
+            lisääNappi = new Button();
+            päivitäNappi = new Button();
+            poistaNappi = new Button();
+            textBox1 = new TextBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // haeNappi
+            // 
+            haeNappi.Location = new Point(159, 59);
+            haeNappi.Name = "haeNappi";
+            haeNappi.Size = new Size(94, 29);
+            haeNappi.TabIndex = 0;
+            haeNappi.Text = "Hae";
+            haeNappi.UseVisualStyleBackColor = true;
+            haeNappi.Click += haeNappi_Click;
+            // 
+            // lisääNappi
+            // 
+            lisääNappi.Location = new Point(28, 119);
+            lisääNappi.Name = "lisääNappi";
+            lisääNappi.Size = new Size(94, 29);
+            lisääNappi.TabIndex = 1;
+            lisääNappi.Text = "Lisää";
+            lisääNappi.UseVisualStyleBackColor = true;
+            lisääNappi.Click += lisääNappi_Click;
+            // 
+            // päivitäNappi
+            // 
+            päivitäNappi.Location = new Point(28, 154);
+            päivitäNappi.Name = "päivitäNappi";
+            päivitäNappi.Size = new Size(94, 29);
+            päivitäNappi.TabIndex = 2;
+            päivitäNappi.Text = "Päivitä";
+            päivitäNappi.UseVisualStyleBackColor = true;
+            päivitäNappi.Click += päivitäNappi_Click;
+            // 
+            // poistaNappi
+            // 
+            poistaNappi.Location = new Point(28, 189);
+            poistaNappi.Name = "poistaNappi";
+            poistaNappi.Size = new Size(94, 29);
+            poistaNappi.TabIndex = 3;
+            poistaNappi.Text = "Poista";
+            poistaNappi.UseVisualStyleBackColor = true;
+            poistaNappi.Click += poistaNappi_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(28, 59);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(268, 59);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(658, 331);
+            dataGridView1.TabIndex = 5;
             // 
             // Asiakkaat
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ButtonShadow;
-            ClientSize = new Size(984, 761);
+            ClientSize = new Size(1125, 1015);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox1);
+            Controls.Add(poistaNappi);
+            Controls.Add(päivitäNappi);
+            Controls.Add(lisääNappi);
+            Controls.Add(haeNappi);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Asiakkaat";
             Text = "Asiakkaat";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button haeNappi;
+        private Button lisääNappi;
+        private Button päivitäNappi;
+        private Button poistaNappi;
+        private TextBox textBox1;
+        private DataGridView dataGridView1;
     }
 }
