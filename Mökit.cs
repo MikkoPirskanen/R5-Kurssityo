888b8f2fd6990a3c61d20cee9ea9062ae1c0a69e
MikkoPirskanen <129946559+MikkoPirskanen@users.noreply.github.com>
2023-04-19 14:57:26 +0300

    Update Mökit.cs

--- Mökit.cs
+++ Mökit.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
-namespace Mökkihöperö
+
+namespace MökkienHallinta
 {
-    public partial class Mökit : Form
+    public partial class Form1 : Form
     {
-        public Mökit()
+        public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnHae_Click(object sender, EventArgs e)
+        {
+            // Hae mökkejä tietokannasta ja näytä ne datagridissä
+        }
+
+        private void btnLisaa_Click(object sender, EventArgs e)
+        {
+            // Avaa lomake uuden mökin lisäämiseksi tietokantaan
+        }
+
+        private void btnMuokkaa_Click(object sender, EventArgs e)
+        {
+            // Avaa lomake valitun mökin muokkaamiseksi tietokannassa
+        }
+
+        private void btnPoista_Click(object sender, EventArgs e)
+        {
+            // Poista valittu mökki tietokannasta
         }
     }
 }
