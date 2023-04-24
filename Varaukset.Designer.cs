7593d7d347606b61ce40b133c0893416646cf636
Mikko <mikko.a.mottonen@edu.savonia.fi>
2023-04-24 15:39:31 +0300

    varausta

--- Varaukset.Designer.cs
+++ Varaukset.Designer.cs
@@ -30,18 +30,20 @@
         {
             label1 = new Label();
             Mökit = new Label();
-            listBox2 = new ListBox();
+            alueenMokit = new ListBox();
             varausKalenteri = new MonthCalendar();
-            comboBox1 = new ComboBox();
+            alueet = new ComboBox();
             dataGridView1 = new DataGridView();
             label2 = new Label();
+            uusiVarausBtn = new Button();
+            label3 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(75, 96);
+            label1.Location = new Point(54, 63);
             label1.Name = "label1";
             label1.Size = new Size(41, 15);
             label1.TabIndex = 1;
@@ -50,70 +52,94 @@
             // Mökit
             // 
             Mökit.AutoSize = true;
-            Mökit.Location = new Point(286, 96);
+            Mökit.Location = new Point(57, 145);
             Mökit.Name = "Mökit";
             Mökit.Size = new Size(38, 15);
             Mökit.TabIndex = 2;
             Mökit.Text = "Mökit";
             // 
-            // listBox2
+            // alueenMokit
             // 
-            listBox2.FormattingEnabled = true;
-            listBox2.ItemHeight = 15;
-            listBox2.Location = new Point(203, 136);
-            listBox2.Name = "listBox2";
-            listBox2.Size = new Size(167, 199);
-            listBox2.TabIndex = 4;
-            listBox2.SelectedIndexChanged += listBox2_SelectedIndexChanged;
+            alueenMokit.FormattingEnabled = true;
+            alueenMokit.ItemHeight = 15;
+            alueenMokit.Location = new Point(17, 181);
+            alueenMokit.Name = "alueenMokit";
+            alueenMokit.Size = new Size(167, 199);
+            alueenMokit.TabIndex = 4;
+            alueenMokit.SelectedIndexChanged += listBox2_SelectedIndexChanged;
             // 
             // varausKalenteri
             // 
             varausKalenteri.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
             varausKalenteri.ForeColor = SystemColors.WindowText;
-            varausKalenteri.Location = new Point(112, 485);
+            varausKalenteri.Location = new Point(57, 436);
             varausKalenteri.MaxDate = new DateTime(2028, 12, 31, 0, 0, 0, 0);
             varausKalenteri.MinDate = new DateTime(2023, 1, 1, 0, 0, 0, 0);
             varausKalenteri.Name = "varausKalenteri";
             varausKalenteri.TabIndex = 5;
+            varausKalenteri.DateChanged += varausKalenteri_DateChanged;
+            varausKalenteri.DateSelected += varausKalenteri_DateSelected;
             // 
-            // comboBox1
+            // alueet
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "Etelä-Savo", "Keski-Suomi", "Pohjois-Savo" });
-            comboBox1.Location = new Point(53, 136);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(121, 23);
-            comboBox1.TabIndex = 6;
-            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged_1;
+            alueet.FormattingEnabled = true;
+            alueet.Items.AddRange(new object[] { "Ahvenanmaa", "Etelä-Karjala", "Etelä-Pohjanmaa", "Etelä-Savo", "Kainuu", "Kanta-Häme", "Keski-Pohjanmaa", "Keski-Suomi", "Kymenlaakso", "Lappi", "Pirkanmaa", "Pohjanmaa", "Pohjois-Karjala", "Pohjois-Pohjanmaa", "Pohjois-Savo", "Päijät-Häme", "Satakunta", "Uusimaa", "Varsinais-Suomi" });
+            alueet.Location = new Point(17, 96);
+            alueet.Name = "alueet";
+            alueet.Size = new Size(121, 23);
+            alueet.TabIndex = 6;
+            alueet.SelectedIndexChanged += comboBox1_SelectedIndexChanged_1;
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(407, 136);
+            dataGridView1.Location = new Point(249, 145);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(545, 390);
+            dataGridView1.Size = new Size(708, 178);
             dataGridView1.TabIndex = 7;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(597, 96);
+            label2.Location = new Point(558, 111);
             label2.Name = "label2";
             label2.Size = new Size(75, 15);
             label2.TabIndex = 8;
             label2.Text = "Mökin tiedot";
+            // 
+            // uusiVarausBtn
+            // 
+            uusiVarausBtn.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            uusiVarausBtn.Location = new Point(519, 611);
+            uusiVarausBtn.Name = "uusiVarausBtn";
+            uusiVarausBtn.Size = new Size(183, 42);
+            uusiVarausBtn.TabIndex = 9;
+            uusiVarausBtn.Text = "Uusi varaus";
+            uusiVarausBtn.UseVisualStyleBackColor = true;
+            uusiVarausBtn.Click += uusiVarausBtn_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(433, 508);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 10;
+            label3.Text = "label3";
             // 
             // Varaukset
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(984, 761);
+            Controls.Add(label3);
+            Controls.Add(uusiVarausBtn);
             Controls.Add(label2);
             Controls.Add(dataGridView1);
-            Controls.Add(comboBox1);
+            Controls.Add(alueet);
             Controls.Add(varausKalenteri);
-            Controls.Add(listBox2);
+            Controls.Add(alueenMokit);
             Controls.Add(Mökit);
             Controls.Add(label1);
             Name = "Varaukset";
@@ -128,10 +154,12 @@
         #endregion
         private Label label1;
         private Label Mökit;
-        private ListBox listBox2;
+        private ListBox alueenMokit;
         private MonthCalendar varausKalenteri;
-        private ComboBox comboBox1;
+        private ComboBox alueet;
         private DataGridView dataGridView1;
         private Label label2;
+        private Button uusiVarausBtn;
+        private Label label3;
     }
 }
--- Varaukset.cs
+++ Varaukset.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Utilities.Collections;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Mökkihöperö
 {
     public partial class Varaukset : Form
     {
+        public int selectedMokkiId = 0;
+
+
+        DateTime startDate;
+        DateTime endDate;
+
+        string connectionString = "server=127.0.0.1; database=vn;uid=root;pwd=VillageNewbies;";
+
 
         public Varaukset()
         {
             InitializeComponent();
+
+
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -25,68 +41,165 @@ namespace Mökkihöperö
 
         private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
         {
+            // Tyhjennä listboxi
+            alueenMokit.Items.Clear();
+
+            // Muodosta tietokantayhteys
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            // Hae valitun alueen mökit
+            string selectedArea = alueet.SelectedItem.ToString();
+
+            // Suorita kysely valitun alueen mökeistä
+            MySqlCommand cmd = new MySqlCommand(
+                "SELECT mokkinimi FROM mokki WHERE alue_id = (SELECT alue_id FROM alue WHERE nimi = @selectedArea)", connection);
+            cmd.Parameters.AddWithValue("@selectedArea", selectedArea);
+
+            // Lue hakutulokset
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
             {
-                // Tyhjennä listboxi
-                listBox2.Items.Clear();
-
-
-
-                // Hae valitun alueen mökit
-                string selectedArea = comboBox1.SelectedItem.ToString();
-                string[] cabins = null;
-
-                if (selectedArea == "Pohjois-Savo")
-                {
-                    cabins = new string[] { "Mökki 1", "Mökki 2", "Mökki 3" };
-                }
-                else if (selectedArea == "Etelä-Savo")
-                {
-                    cabins = new string[] { "Mökki 4", "Mökki 5", "Mökki 6" };
-                }
-
-                // Lisää mökit listboxiin
-                if (cabins != null)
-                {
-                    foreach (string cabin in cabins)
-                    {
-                        listBox2.Items.Add(cabin);
-                    }
-                }
+                string mokkiNimi = reader.GetString("mokkinimi");
+                alueenMokit.Items.Add(mokkiNimi);
             }
 
+            // Sulje lukija ja tietokantayhteys
+            reader.Close();
+            connection.Close();
         }
+
+
 
         private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Dictionary<string, Dictionary<string, string>> cabins = new Dictionary<string, Dictionary<string, string>>();
-            cabins.Add("Mökki 1", new Dictionary<string, string>() { { "Osoite", "Katu 1" }, { "Hinta", "100€/vrk" }, { "Kuvaus", "Tunnelmallinen hirsimökki" } });
-            cabins.Add("Mökki 2", new Dictionary<string, string>() { { "Osoite", "Katu 2" }, { "Hinta", "150€/vrk" }, { "Kuvaus", "Iso ja moderni huvila" } });
-            cabins.Add("Mökki 3", new Dictionary<string, string>() { { "Osoite", "Katu 3" }, { "Hinta", "200€/vrk" }, { "Kuvaus", "Tyylikäs huvila" } });
-            cabins.Add("Mökki 4", new Dictionary<string, string>() { { "Osoite", "Katu 4" }, { "Hinta", "250€/vrk" }, { "Kuvaus", "Tyylikäs huvila" } });
-            cabins.Add("Mökki 5", new Dictionary<string, string>() { { "Osoite", "Katu 5" }, { "Hinta", "300€/vrk" }, { "Kuvaus", "Tyylikäs huvila" } });
-            cabins.Add("Mökki 6", new Dictionary<string, string>() { { "Osoite", "Katu 6" }, { "Hinta", "350€/vrk" }, { "Kuvaus", "Tyylikäs huvila" } });
 
+            // Muodosta tietokantayhteys
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
 
             // Hae valitun mökin tiedot
-            string selectedCabin = listBox2.SelectedItem.ToString();
-            Dictionary<string, string> cabinInfo = cabins[selectedCabin];
 
-            // Näytä tiedot datagridviewissä
+            string selectedCabin = alueenMokit.SelectedItem.ToString();
+            MySqlCommand cmd = new MySqlCommand(
+                "SELECT * FROM mokki WHERE mokkinimi = @selectedCabin", connection);
+            cmd.Parameters.AddWithValue("@selectedCabin", selectedCabin);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
             dataGridView1.Rows.Clear();
-            foreach (KeyValuePair<string, string> kvp in cabinInfo)
+            dataGridView1.Columns.Clear();
+
+            dataGridView1.Columns.Add("mokkinimi", "Mökin nimi");
+            dataGridView1.Columns.Add("katuosoite", "Katuosoite");
+            dataGridView1.Columns.Add("hinta", "Hinta");
+            dataGridView1.Columns.Add("kuvaus", "Kuvaus");
+            dataGridView1.Columns.Add("henkilomaara", "Henkilömäärä");
+            dataGridView1.Columns.Add("varustelu", "Varustelu");
+
+
+            while (reader.Read())
             {
-                dataGridView1.Columns.Add(kvp.Key, kvp.Value);
-                dataGridView1.Rows.Add(kvp.Key, kvp.Value);
-                         
+                selectedMokkiId = reader.GetInt32("mokki_id");
+                string mokkiNimi = reader.GetString("mokkinimi");
+                string katuosoite = reader.GetString("katuosoite");
+                string hinta = reader.GetString("hinta");
+                string kuvaus = reader.GetString("kuvaus");
+                string henkilomaara = reader.GetString("henkilomaara");
+                string varustelu = reader.GetString("varustelu");
+                string[] row = new string[] { mokkiNimi, katuosoite, hinta, kuvaus, henkilomaara, varustelu };
+                dataGridView1.Rows.Add(row);
+
+                  }
+
+            reader.Close();
+
+
+
+            string query = "SELECT * FROM varaus WHERE mokki_mokki_id = 2";
+            MySqlCommand cmdx = new MySqlCommand(query, connection);
+            cmdx.Parameters.AddWithValue("@mokki_mokki_id", selectedMokkiId);
+            cmdx.Parameters.AddWithValue("@varattu_alkupvm", startDate.ToString("yyyy-MM-dd"));
+            cmdx.Parameters.AddWithValue("@varattu_loppupvm", endDate.ToString("yyyy-MM-dd"));
+
+          cmdx.ExecuteReader();
+
+            // Oletetaan, että varauksen alku- ja loppupäivämäärät ovat tallennettu muuttujiin startDate ja endDate.
+
+            label1.Text = startDate.ToString("yyyy-MM-dd");
+
+
+            // Määritä valittu alue kalenterissa
+          //  varausKalenteri.SelectionStart = startDate;
+           // varausKalenteri.SelectionEnd = endDate;
+
+            // Lisää jokainen päivämäärä varattujen päivämäärien joukkoon
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                varausKalenteri.AddBoldedDate(date);
             }
+
+            // Värjää kalenterissa olevat päivät, jotka ovat varattuja
+            varausKalenteri.UpdateBoldedDates();
+
+          
+
+
+            // Sulje lukija ja tietokantayhteys
+            reader.Close();
+            connection.Close();
+
+
         }
 
-        
 
 
 
-private void Varaukset_Load(object sender, EventArgs e)
+
+        private void Varaukset_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void uusiVarausBtn_Click(object sender, EventArgs e)
+        {
+            // Tällä voit laittaa tietokantaan testidataa
+
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            MySqlCommand xxx = new MySqlCommand("INSERT INTO posti (postinro, toimipaikka) VALUES (50000, 'MIKKELI')", connection);
+
+            xxx.ExecuteNonQuery();
+
+            MySqlCommand cmd = new MySqlCommand("REPLACE INTO mokki (mokki_id, alue_id, postinro, mokkinimi, katuosoite, hinta, kuvaus, henkilomaara, varustelu) " +
+                                      "VALUES (1, 12, (SELECT postinro FROM posti WHERE toimipaikka = 'MIKKELI'), 'Villa rötiskö', 'Katuosoite 2', 500, 'Upea huvila kaikilla herkuilla', 8, 'Keittiö ja sauna löytyy joo')", connection);
+
+
+            cmd.ExecuteNonQuery();
+            /*
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO alue (alue_id, nimi) VALUES (10,'Etelä-Karjala'), (11,'Etelä-Pohjanmaa'), (12,'Etelä-Savo'), (13,'Kainuu'), (14,'Kanta-Häme'), (15,'Keski-Pohjanmaa'), (16,'Keski-Suomi'), (17,'Kymenlaakso'), (18,'Lappi'), (19,'Pirkanmaa'), (20,'Pohjanmaa'), (21,'Pohjois-Karjala'), (22,'Pohjois-Pohjanmaa'), (23,'Pohjois-Savo'), (24,'Päijät-Häme'), (25,'Satakunta'), (26,'Uusimaa'), (27,'Varsinais-Suomi')", connection);
+            cmd.ExecuteNonQuery();
+             */
+
+
+
+
+
+
+
+        }
+
+        private void varausKalenteri_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void varausKalenteri_DateSelected(object sender, DateRangeEventArgs e)
+        {
+           
+
+
 
         }
     }
