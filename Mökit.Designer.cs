fc8b818d093f8e606fe3eec9803375ddb1c00d4a
MikkoPirskanen <129946559+MikkoPirskanen@users.noreply.github.com>
2023-04-27 10:49:40 +0300

    Update Mökit.Designer.cs

--- Mökit.Designer.cs
+++ Mökit.Designer.cs
@@ -1,174 +1,222 @@
-neamespace Mökkihöperö
+namespace Mökkihöperö
 {
     partial class Mokit
-{
-
-    /// <summary>
-    /// Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
     {
-        if (disposing && (components != null))
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
         {
-            components.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
-        base.Dispose(disposing);
-    }
 
-    private void InitializeComponent()
-    {
-        button1 = new Button();
-        textBox1 = new TextBox();
-        dataGridView1 = new DataGridView();
-        button2 = new Button();
-        button3 = new Button();
-        button4 = new Button();
-        monthCalendar1 = new MonthCalendar();
-        label1 = new Label();
-        button5 = new Button();
-        button6 = new Button();
-        button7 = new Button();
-        button8 = new Button();
-        ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-        this.SuspendLayout();
-        // 
-        // button1
-        // 
-        button1.Location = new Point(244, 43);
-        button1.Name = "button1";
-        button1.Size = new Size(94, 29);
-        button1.TabIndex = 0;
-        button1.Text = "Haku";
-        button1.UseVisualStyleBackColor = true;
-        // 
-        // textBox1
-        // 
-        textBox1.Location = new Point(42, 43);
-        textBox1.Name = "textBox1";
-        textBox1.Size = new Size(196, 27);
-        textBox1.TabIndex = 1;
-        textBox1.Text = "Hakukenttä";
-        // 
-        // dataGridView1
-        // 
-        dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-        dataGridView1.Location = new Point(42, 88);
-        dataGridView1.Name = "dataGridView1";
-        dataGridView1.RowHeadersWidth = 51;
-        dataGridView1.RowTemplate.Height = 29;
-        dataGridView1.Size = new Size(1031, 385);
-        dataGridView1.TabIndex = 2;
-        // 
-        // button2
-        // 
-        button2.Location = new Point(440, 43);
-        button2.Name = "button2";
-        button2.Size = new Size(94, 29);
-        button2.TabIndex = 3;
-        button2.Text = "Perustiedot";
-        button2.UseVisualStyleBackColor = true;
-        // 
-        // button3
-        // 
-        button3.Location = new Point(540, 43);
-        button3.Name = "button3";
-        button3.Size = new Size(129, 29);
-        button3.TabIndex = 4;
-        button3.Text = "Osoitetiedot";
-        button3.UseVisualStyleBackColor = true;
-        // 
-        // button4
-        // 
-        button4.Location = new Point(675, 43);
-        button4.Name = "button4";
-        button4.Size = new Size(157, 29);
-        button4.TabIndex = 5;
-        button4.Text = "Kuvaus ja Varustelu";
-        button4.UseVisualStyleBackColor = true;
-        // 
-        // monthCalendar1
-        // 
-        monthCalendar1.Location = new Point(1111, 125);
-        monthCalendar1.Name = "monthCalendar1";
-        monthCalendar1.TabIndex = 6;
-        // 
-        // label1
-        // 
-        label1.AutoSize = true;
-        label1.Location = new Point(1167, 90);
-        label1.Name = "label1";
-        label1.Size = new Size(97, 20);
-        label1.TabIndex = 7;
-        label1.Text = "Varaustilanne";
-        // 
-        // button5
-        // 
-        button5.Location = new Point(1111, 344);
-        button5.Name = "button5";
-        button5.Size = new Size(220, 29);
-        button5.TabIndex = 8;
-        button5.Text = "Varaa mökki";
-        button5.UseVisualStyleBackColor = true;
-        // 
-        // button6
-        // 
-        button6.Location = new Point(42, 490);
-        button6.Name = "button6";
-        button6.Size = new Size(94, 29);
-        button6.TabIndex = 9;
-        button6.Text = "Lisää Mökki";
-        button6.UseVisualStyleBackColor = true;
-        // 
-        // button7
-        // 
-        button7.Location = new Point(142, 490);
-        button7.Name = "button7";
-        button7.Size = new Size(147, 29);
-        button7.TabIndex = 10;
-        button7.Text = "Muokkaa Mökkiä";
-        button7.UseVisualStyleBackColor = true;
-        // 
-        // button8
-        // 
-        button8.Location = new Point(295, 490);
-        button8.Name = "button8";
-        button8.Size = new Size(146, 29);
-        button8.TabIndex = 11;
-        button8.Text = "Poista Mökki";
-        button8.UseVisualStyleBackColor = true;
-        // 
-        // Mokit
-        // 
-        this.ClientSize = new Size(1349, 550);
-        this.Controls.Add(button8);
-        this.Controls.Add(button7);
-        this.Controls.Add(button6);
-        this.Controls.Add(button5);
-        this.Controls.Add(label1);
-        this.Controls.Add(monthCalendar1);
-        this.Controls.Add(button4);
-        this.Controls.Add(button3);
-        this.Controls.Add(button2);
-        this.Controls.Add(dataGridView1);
-        this.Controls.Add(textBox1);
-        this.Controls.Add(button1);
-        this.Name = "Mokit";
-        ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-        this.ResumeLayout(false);
-        this.PerformLayout();
-    }
+        #region Windows Form Designer generated code
 
-    private Button button1;
-    private TextBox textBox1;
-    private DataGridView dataGridView1;
-    private Button button2;
-    private Button button3;
-    private Button button4;
-    private MonthCalendar monthCalendar1;
-    private Label label1;
-    private Button button5;
-    private Button button6;
-    private Button button7;
-    private Button button8;
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            dataGridView1 = new DataGridView();
+            monthCalendar1 = new MonthCalendar();
+            label1 = new Label();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(37, 33);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(225, 27);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Hakukenttä";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(268, 33);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Haku";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(521, 31);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 2;
+            button2.Text = "Perustiedot";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(621, 31);
+            button3.Name = "button3";
+            button3.Size = new Size(130, 29);
+            button3.TabIndex = 3;
+            button3.Text = "Osoitetiedot";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(757, 31);
+            button4.Name = "button4";
+            button4.Size = new Size(196, 29);
+            button4.TabIndex = 4;
+            button4.Text = "Kuvaus ja varustelu";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 68);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(1033, 494);
+            dataGridView1.TabIndex = 5;
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Location = new Point(1086, 126);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(1145, 97);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Varaustilanne";
+            // 
+            // button5
+            // 
+            button5.Location = new Point(1086, 355);
+            button5.Name = "button5";
+            button5.Size = new Size(220, 29);
+            button5.TabIndex = 8;
+            button5.Text = "Tee varaus";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(12, 568);
+            button6.Name = "button6";
+            button6.Size = new Size(128, 29);
+            button6.TabIndex = 9;
+            button6.Text = "Lisää mökki";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(146, 568);
+            button7.Name = "button7";
+            button7.Size = new Size(166, 29);
+            button7.TabIndex = 10;
+            button7.Text = "Muokkaa mökkiä";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(318, 568);
+            button8.Name = "button8";
+            button8.Size = new Size(134, 29);
+            button8.TabIndex = 11;
+            button8.Text = "Poista mökki";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(669, 568);
+            button9.Name = "button9";
+            button9.Size = new Size(120, 29);
+            button9.TabIndex = 12;
+            button9.Text = "Lisää alue";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(795, 568);
+            button10.Name = "button10";
+            button10.Size = new Size(140, 29);
+            button10.TabIndex = 13;
+            button10.Text = "Muokkaa aluetta";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(941, 568);
+            button11.Name = "button11";
+            button11.Size = new Size(104, 29);
+            button11.TabIndex = 14;
+            button11.Text = "Poista alue";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // Mokit
+            // 
+            ClientSize = new Size(1353, 636);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(label1);
+            Controls.Add(monthCalendar1);
+            Controls.Add(dataGridView1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Mokit";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private DataGridView dataGridView1;
+        private MonthCalendar monthCalendar1;
+        private Label label1;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+    }
 }
