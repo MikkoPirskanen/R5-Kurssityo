3c6e72b9ddb88423f6fa662573ceb5c200cb4d8e
Eetuher <130357435+Eetuher@users.noreply.github.com>
2023-05-11 02:26:20 +0300

    KYLLÄ

---
+++ UusiMökki.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Mökkihöperö
+{
+    partial class UusiMökki
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            valitseHinta = new TextBox();
+            valitseNimi = new TextBox();
+            valitseKuvaus = new TextBox();
+            button1 = new Button();
+            valitseAlue2 = new ComboBox();
+            valitseVarustelu = new TextBox();
+            valitsePostinro = new TextBox();
+            valitseKatuosoite = new TextBox();
+            valitseHlomaara = new TextBox();
+            label1 = new Label();
+            Nimi = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            SuspendLayout();
+            // 
+            // valitseHinta
+            // 
+            valitseHinta.Location = new Point(197, 218);
+            valitseHinta.Name = "valitseHinta";
+            valitseHinta.Size = new Size(182, 31);
+            valitseHinta.TabIndex = 3;
+            valitseHinta.TextChanged += valitseHinta_TextChanged_1;
+            valitseHinta.KeyPress += valitseHinta_KeyPress_1;
+            // 
+            // valitseNimi
+            // 
+            valitseNimi.Location = new Point(197, 165);
+            valitseNimi.Name = "valitseNimi";
+            valitseNimi.Size = new Size(182, 31);
+            valitseNimi.TabIndex = 4;
+            // 
+            // valitseKuvaus
+            // 
+            valitseKuvaus.Location = new Point(197, 275);
+            valitseKuvaus.Name = "valitseKuvaus";
+            valitseKuvaus.Size = new Size(473, 31);
+            valitseKuvaus.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(197, 379);
+            button1.Name = "button1";
+            button1.Size = new Size(141, 47);
+            button1.TabIndex = 10;
+            button1.Text = "Lisää mökki";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // valitseAlue2
+            // 
+            valitseAlue2.FormattingEnabled = true;
+            valitseAlue2.Location = new Point(197, 110);
+            valitseAlue2.Name = "valitseAlue2";
+            valitseAlue2.Size = new Size(182, 33);
+            valitseAlue2.TabIndex = 11;
+            valitseAlue2.DropDown += valitseAlue2_DropDown_1;
+            valitseAlue2.SelectedIndexChanged += valitseAlue2_SelectedIndexChanged;
+            // 
+            // valitseVarustelu
+            // 
+            valitseVarustelu.Location = new Point(197, 324);
+            valitseVarustelu.Name = "valitseVarustelu";
+            valitseVarustelu.Size = new Size(473, 31);
+            valitseVarustelu.TabIndex = 12;
+            // 
+            // valitsePostinro
+            // 
+            valitsePostinro.Location = new Point(488, 218);
+            valitsePostinro.MaxLength = 5;
+            valitsePostinro.Name = "valitsePostinro";
+            valitsePostinro.Size = new Size(182, 31);
+            valitsePostinro.TabIndex = 13;
+            valitsePostinro.KeyPress += valitsePostinro_KeyPress;
+            // 
+            // valitseKatuosoite
+            // 
+            valitseKatuosoite.Location = new Point(488, 165);
+            valitseKatuosoite.MaxLength = 30;
+            valitseKatuosoite.Name = "valitseKatuosoite";
+            valitseKatuosoite.Size = new Size(182, 31);
+            valitseKatuosoite.TabIndex = 14;
+            valitseKatuosoite.TextChanged += valitseKatuosoite_TextChanged;
+            // 
+            // valitseHlomaara
+            // 
+            valitseHlomaara.Location = new Point(488, 112);
+            valitseHlomaara.MaxLength = 5;
+            valitseHlomaara.Name = "valitseHlomaara";
+            valitseHlomaara.Size = new Size(182, 31);
+            valitseHlomaara.TabIndex = 15;
+            valitseHlomaara.KeyPress += valitseHlomaara_KeyPress;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(113, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 25);
+            label1.TabIndex = 16;
+            label1.Text = "Alue";
+            // 
+            // Nimi
+            // 
+            Nimi.AutoSize = true;
+            Nimi.Location = new Point(113, 165);
+            Nimi.Name = "Nimi";
+            Nimi.Size = new Size(49, 25);
+            Nimi.TabIndex = 17;
+            Nimi.Text = "Nimi";
+            Nimi.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(113, 273);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 25);
+            label3.TabIndex = 19;
+            label3.Text = "Kuvaus";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(113, 218);
+            label4.Name = "label4";
+            label4.Size = new Size(54, 25);
+            label4.TabIndex = 18;
+            label4.Text = "Hinta";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(393, 218);
+            label5.Name = "label5";
+            label5.Size = new Size(77, 25);
+            label5.TabIndex = 23;
+            label5.Text = "Postinro";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(386, 165);
+            label6.Name = "label6";
+            label6.Size = new Size(96, 25);
+            label6.TabIndex = 22;
+            label6.Text = "Katuosoite";
+            label6.Click += label6_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(393, 113);
+            label7.Name = "label7";
+            label7.Size = new Size(89, 25);
+            label7.TabIndex = 21;
+            label7.Text = "Hlömäärä";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(107, 324);
+            label8.Name = "label8";
+            label8.Size = new Size(84, 25);
+            label8.TabIndex = 20;
+            label8.Text = "Varustelu";
+            // 
+            // UusiMökki
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Controls.Add(Nimi);
+            Controls.Add(label1);
+            Controls.Add(valitseHlomaara);
+            Controls.Add(valitseKatuosoite);
+            Controls.Add(valitsePostinro);
+            Controls.Add(valitseVarustelu);
+            Controls.Add(valitseAlue2);
+            Controls.Add(button1);
+            Controls.Add(valitseKuvaus);
+            Controls.Add(valitseNimi);
+            Controls.Add(valitseHinta);
+            Name = "UusiMökki";
+            Text = "UusiMökki";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox valitseHinta;
+        private TextBox valitseNimi;
+        private TextBox valitseKuvaus;
+        private Button button1;
+        private ComboBox valitseAlue2;
+        private TextBox valitseVarustelu;
+        private TextBox valitsePostinro;
+        private TextBox valitseKatuosoite;
+        private TextBox valitseHlomaara;
+        private Label label1;
+        private Label Nimi;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+    }
+}
---
+++ UusiMökki.cs
@@ -0,0 +1,210 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mökkihöperö
+{
+    public partial class UusiMökki : Form
+    {
+        List<string> alueIDseuranta2 = new List<string>();
+        string connectionString = "server=127.0.0.1; database=vn;uid=root;pwd=VillageNewbies;";
+        public UusiMökki()
+        {
+            InitializeComponent();
+
+            // Suljetaan yhteys
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void valitseHinta_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void valitseHinta_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+            (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as System.Windows.Forms.TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void valitseAlue2_DropDown(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+        private void valitseAlue2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Random rnd = new Random();
+                int idintrandomed = rnd.Next(10000, 99999);
+                string idstringrandomed = idintrandomed.ToString();
+
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                connection.Open();
+                
+                double APhinta = double.Parse(valitseHinta.Text);
+                int postinro = int.Parse(valitsePostinro.Text);
+
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO mokki (mokki_id, alue_id, postinro, mokkinimi, katuosoite, hinta, kuvaus, henkilomaara, varustelu) VALUES (@mokki_id, @alue_id, @postinro, @mokkinimi, @katuosoite, @hinta, @kuvaus, @henkilomaara, @varustelu)");
+                
+                cmd.Connection = connection;
+                cmd.Parameters.AddWithValue("@mokki_id", idintrandomed);
+                cmd.Parameters.AddWithValue("@alue_id", alueIDseuranta2[valitseAlue2.SelectedIndex]);
+                cmd.Parameters.AddWithValue("@postinro", valitsePostinro.Text);
+                cmd.Parameters.AddWithValue("@mokkinimi", valitseNimi.Text);
+                cmd.Parameters.AddWithValue("@katuosoite", valitseKatuosoite.Text);
+                cmd.Parameters.AddWithValue("@hinta", APhinta);
+                cmd.Parameters.AddWithValue("@kuvaus", valitseKuvaus.Text);
+                cmd.Parameters.AddWithValue("@henkilomaara", valitseHlomaara.Text);
+                cmd.Parameters.AddWithValue("@varustelu", valitseVarustelu.Text);
+                
+                cmd.ExecuteNonQuery();
+
+
+                MessageBox.Show("Mökin lisätty onnistuneesti!");
+                connection.Close();
+
+                this.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe lisätessä mökkiä tietokantaan: " + ex.Message);
+            }
+        }
+
+        private void valitseHinta_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void valitsePostinro_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+            /* only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as System.Windows.Forms.TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }*/
+        }
+
+        private void valitseKatuosoite_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void valitseHlomaara_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void valitseAlue2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void valitseAlue2_DropDown_1(object sender, EventArgs e)
+        {
+            // Tyhjennä listboxi
+            valitseAlue2.Items.Clear();
+
+
+
+            // Muodosta tietokantayhteys
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            // Suorita kysely valitun alueen mökeistä
+            MySqlCommand cmd = new MySqlCommand(
+                "SELECT nimi, alue_id FROM alue", connection);
+
+
+
+            // Lue hakutulokset
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string mokkiNimi = reader.GetString("nimi");
+                alueIDseuranta2.Add(reader.GetString("alue_id"));
+                valitseAlue2.Items.Add(mokkiNimi);
+            }
+
+            // Sulje lukija ja tietokantayhteys
+            reader.Close();
+            connection.Close();
+        }
+
+        private void valitseHinta_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+            (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as System.Windows.Forms.TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
